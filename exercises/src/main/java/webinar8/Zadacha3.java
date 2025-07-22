53f3adaa57bd95681e924616254f4c263c519c80
CherepovAlex <westler123@yandex.ru>
2025-07-22 19:59:02 +0300

    commit DZ webinar 8

--- exercises/src/main/java/webinar8/Zadacha3.java
+++ exercises/src/main/java/webinar8/Zadacha3.java
@@ -2,15 +2,23 @@ package webinar8;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Zadacha3 {
 
     public static void main(String[] args) {
-
+        // 1 вариант
         String word1 = generateWord();
         String word2 = generateWord();
-        System.out.println("Сгенерированные слова: " + word1 + " " + word2);
+        System.out.println("Сгенерированная строка: " + word1 + " " + word2);
         System.out.println("Вывод новой строки: " + sortWord(word1) + " " + sortWord(word2));
+        // 2 вариант
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите два слова через пробел: ");
+        String input = scanner.nextLine();
+        String[] words = input.split(" ");
+        System.out.println(sortWord(words[0]) + " " + sortWord(words[1]));
+
     }
 
     public static String generateWord() {
